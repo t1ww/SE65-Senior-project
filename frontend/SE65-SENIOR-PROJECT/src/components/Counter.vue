a8f78b3d4f56f295fdc7cfab75adf22461c58d4a
penplub <ocpe278@gmail.com>
2025-02-07 10:10:46 +0700

    Open and create project

---
+++ frontend/SE65-SENIOR-PROJECT/src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+<template>
+     <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+</template>
---
+++ frontend/SE65-SENIOR-PROJECT/src/page/HelloWorld.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Counter from "../components/Counter.vue"
+import Footer from "../components/Footer.vue"
+
+defineProps<{ msg: string }>()
+
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+ <Counter>
+  
+ </Counter>
+ <Footer>
+  
+ </Footer>
+
+  
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
