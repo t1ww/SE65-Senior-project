c03d8e81a68bb9881fbf9341065292a5b93a49ef
t1ww <95153968+t1ww@users.noreply.github.com>
2025-02-18 15:19:30 +0700

    Update Lecture.vue

--- frontend/SE65-SENIOR-PROJECT/src/pages/Lecture.vue
+++ frontend/SE65-SENIOR-PROJECT/src/pages/Lecture.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { useRouter, type RouteLocationAsPathGeneric, type RouteLocationAsRelativeGeneric } from 'vue-router';
 const router = useRouter();
 
-const navigateTo = (path) => {
+const navigateTo = (path: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric) => {
   router.push(path);
 };
 </script>
