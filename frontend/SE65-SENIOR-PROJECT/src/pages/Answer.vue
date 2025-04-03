42843c411859fe38d5be9da6a1fa56ffb84be00c
Phiriya652115030 <153396760+Phiriya652115030@users.noreply.github.com>
2025-04-03 14:10:03 +0700

    remove button

--- frontend/SE65-SENIOR-PROJECT/src/pages/Answer.vue
+++ frontend/SE65-SENIOR-PROJECT/src/pages/Answer.vue
@@ -16,9 +16,6 @@ const code = ref('');
   <div class="answer-page container-fluid d-flex align-items-center justify-content-center min-vh-100">
     <div class="card p-4 rounded shadow" style="max-width: 600px; width: 100%;">
       <div class="d-flex justify-content-between align-items-center mb-3">
-        <button class="btn btn-link">
-          <i class="bi bi-arrow-left-circle" style="font-size: 1.5rem;"></i>
-        </button>
         <h4 class="text-center w-100 text-purple">Question: {{ question.name }}</h4>
         <i class="bi bi-person-circle" style="font-size: 1.5rem;"></i>
       </div>
