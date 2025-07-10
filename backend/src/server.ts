f385961906ae1c9351370c8cbf8c73354bc5a3bc
t1ww <95153968+t1ww@users.noreply.github.com>
2025-07-10 23:01:54 +0700

    fixing backend + throwing env errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,16 +8,28 @@ import answerRoutes from "./routes/answers"
 dotenv.config();
 
 const app = express();
-const BACKEND_PORT = process.env.BACKEND_PORT;
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN;
+if (!process.env.FRONTEND_ORIGIN) {
+  throw new Error("FRONTEND_ORIGIN environment variable is required");
+}
+
+const BACKEND_PORT = Number(process.env.BACKEND_PORT);
+if (!process.env.BACKEND_PORT) {
+  throw new Error("BACKEND_PORT environment variable is required");
+}
+if (isNaN(BACKEND_PORT)) {
+  throw new Error("BACKEND_PORT must be a valid number");
+}
 
 app.use(cors({
-  origin: `http://localhost:${process.env.FRONTEND_PORT}`
+  origin: FRONTEND_ORIGIN
 }));
 app.use(express.json()); // Parse JSON
 app.use("/users", userRoutes); // Mount user routes
 app.use("/questions", questionRoutes);
 app.use("/answers", answerRoutes);
 
-app.listen(BACKEND_PORT, () => {
+
+app.listen(BACKEND_PORT, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${BACKEND_PORT}`);
 });
